443f617c39a9e447e581fcd76a25db8e3c2b4c9e
skyewong <93854496+SkyeWong@users.noreply.github.com>
2022-04-23 13:44:49 +0800

    Update dev_only.py

--- cogs/dev_only.py
+++ cogs/dev_only.py
@@ -25,40 +25,6 @@ class dev_only(commands.Cog, name='Tests Commands'):
     @commands.command(name="whatsthetime", brief="Shows the time.")
     async def whatsthetime(self, ctx):
         await ctx.send(f"<t:{int(datetime.now().timestamp())}>")
-
-    @commands.command(name="texttable", brief="example of texttable module.")
-    async def texttable(self, ctx):
-        table = Texttable()
-        table.set_cols_align(["l", "r", "c"])
-        table.set_cols_valign(["t", "m", "b"])
-        table.add_rows([["Name", "Age", "Nickname"],
-                        ["Mr\nXavier\nHuon", 32, "Xav'"],
-                        ["Mr\nBaptiste\nClement", 1, "Baby"],
-                        ["Mme\nLouise\nBourgeau", 28, "Lou\n\nLoue"]])
-        await ctx.send(f"```ml\n{table.draw()}```")
-        await ctx.send("** **")
-        table = Texttable()
-        table.set_deco(Texttable.HEADER)
-        table.set_cols_dtype(['t',  # text
-                              'f',  # float (decimal)
-                              'e',  # float (exponent)
-                              'i',  # integer
-                              'a']) # automatic
-        table.set_cols_align(["l", "r", "r", "r", "l"])
-        table.add_rows([["text",    "float", "exp", "int", "auto"],
-                        ["abcd",    "67",    654,   89,    128.001],
-                        ["efghijk", 67.5434, .654,  89.6,  12800000000000000000000.00023],
-                        ["lmn",     5e-78,   5e-78, 89.4,  .000000000000128],
-                ["opqrstu", .023,    5e+78, 92.,   12800000000000000000000]])
-        await ctx.send(f"```ml\n{table.draw()}```")
-    
-    @commands.command(name="usersview")
-    async def usersview(self, ctx):
-        await ctx.send('Fetching results...')
-        sql = "SELECT * from users"
-        cursor = db.execute(sql)
-        for row in cursor.fetchall():
-                await ctx.send(row)
         
 def setup(bot: commands.Bot):
     bot.add_cog(dev_only(bot))
